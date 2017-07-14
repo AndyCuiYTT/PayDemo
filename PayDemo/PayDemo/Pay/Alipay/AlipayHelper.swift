61bba0ae5ccab0cd84fb207e9b5e230bf9614c82
Andy <colin@iOSdeMacBook-Air.local>
2017-07-14 13:48:06 +0800

    集成 alipay,wxpay

---
+++ PayDemo/PayDemo/Pay/Alipay/AlipayHelper.swift
@@ -0,0 +1,83 @@
+//
+//  AlipayHelper.swift
+//  PayDemo
+//
+//  Created by Andy on 2017/7/14.
+//  Copyright © 2017年 AndyCuiYTT. All rights reserved.
+//
+
+/**
+ *  author: AndyCUi
+ *
+ *  date: 2017-7-14
+ *
+ *  description: 封装支付方法,支付结果通过代理回调处理.
+ *
+ *  note: swift 集成 ailpay 需要建立桥接文件添加 '#import <AlipaySDK/AlipaySDK.h>',如果 Unknown type name 'UIWindow' 或 property with 'weak' attribute must be of object type 错误请添加 '#import <UIKit/UIKit.h>',务必添加在 '#import <AlipaySDK/AlipaySDK.h>' 之前
+ *
+ *
+ */
+
+
+protocol AlipayDelegate: class {
+    
+    /// 支付成功
+    func alipaySuccess(_ result: Any) -> Void;
+    
+    /// 支付失败
+    func alipayFail(_ result: Any) -> Void;
+    
+    /// 支付取消
+    func alipayCancel(_ result: Any) -> Void;
+    
+    /// 其他未知错误
+    func alipayUnknownError(_ result: Any) -> Void;
+    
+}
+
+
+
+import UIKit
+
+class AlipayHelper: NSObject {
+    
+    private weak var delegate: AlipayDelegate?
+    
+    init(_ delegate: AlipayDelegate) {
+        super.init()
+        self.delegate = delegate
+    }
+    
+    
+    /// 调起支付宝支付
+    ///
+    /// - Parameters:
+    ///   - orderinfo: 商品信息字符串
+    ///   - signedString: 商户信息签名
+    ///   - fromScheme: 应用注册scheme
+    ///   - resultDic: 支付结果回调
+    func pay(_ orderinfo: String, signedString: String, fromScheme: String) -> Void {
+        let payOrder = "\(orderinfo)&sign=\(signedString)"
+        AlipaySDK.defaultService().payOrder(payOrder, fromScheme: fromScheme){ (result) in
+            
+            switch result?["code"] as! String {
+            case "9000":
+                self.delegate?.alipaySuccess(result!)
+            case "4000":
+                self.delegate?.alipayFail(result!)
+            case "6001":
+                self.delegate?.alipayCancel(result!)
+            default:
+                self.delegate?.alipayUnknownError(result!)
+            }
+        }
+    }
+    
+    class func pay() -> Void{
+        
+    }
+    
+    
+    
+
+}
---
+++ PayDemo/PayDemo/Pay/WeChatPay/WeChatPayHelper.swift
@@ -0,0 +1,90 @@
+//
+//  WeChatPayHelper.swift
+//  PayDemo
+//
+//  Created by Andy on 2017/7/14.
+//  Copyright © 2017年 AndyCuiYTT. All rights reserved.
+//
+
+/**
+ *  author: AndyCUi
+ *
+ *  date: 2017-7-14
+ *
+ *  description: 封装支付方法,支付结果通过代理回调处理.
+ *
+ *  note: 如果需要使用 cocopods 导入,在 Podfile 添加 pod 'WechatOpenSDK'
+ *
+ */
+
+
+import UIKit
+
+protocol WeChatPayDelegate : class{
+    
+    /// 微信未安装
+    func WeChatPayWXAppUninstall() -> Void;
+    
+    /// 支付成功
+    func WeChatPaySuccess() -> Void;
+    
+    /// 支付失败
+    func WeChatPayFail(errStr: String) -> Void;
+    
+    /// 支付取消
+    func WeChatPayCancel() -> Void;
+}
+
+class WeChatPayHelper: NSObject, WXApiDelegate {
+    
+    private weak var delegate: WeChatPayDelegate?
+    
+    
+    override init() {
+        super.init()
+    }
+    
+    
+    init(_ delegate: WeChatPayDelegate) {
+        super.init()
+        self.delegate = delegate
+    }
+    
+    func pay(_ orderInfo: [String : String]) -> Void {
+        
+        if WXApi.isWXAppInstalled() {
+            let request = PayReq()
+            request.partnerId = orderInfo["partnerId"]
+            request.prepayId = orderInfo["prepayId"]
+            request.package = orderInfo["package"]
+            request.nonceStr = orderInfo["nonceStr"]
+            request.timeStamp = UInt32(orderInfo["timeStamp"]!)!
+            request.sign = orderInfo["sign"]
+            WXApi.send(request)
+        }else {
+            delegate?.WeChatPayWXAppUninstall()
+        }
+    }
+    
+    
+    func onResp(_ resp: BaseResp!) {
+        if let response = resp as? PayResp {
+            switch response.errCode {
+            case WXSuccess.rawValue:
+                delegate?.WeChatPaySuccess()
+            case WXErrCodeUserCancel.rawValue:
+                delegate?.WeChatPayCancel()
+            default:
+                delegate?.WeChatPayFail(errStr: response.errStr)
+            }
+        }
+    }
+    
+    
+    func onReq(_ req: BaseReq!) {
+        
+    }
+    
+    
+    
+}
